ea71563029fd058100161a7ba9c306183aa3ff99
raonpark <sumin9278@gmail.com>
2023-10-18 10:59:52 +0900

    init

---
+++ src/main/kotlin/com/example/guesstheprice/GuessthepriceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.guesstheprice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuessthepriceApplication
+
+fun main(args: Array<String>) {
+    runApplication<GuessthepriceApplication>(*args)
+}
